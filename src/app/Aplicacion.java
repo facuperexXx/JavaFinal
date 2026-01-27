61458578092cb25e566fa9e7d22ddc9b904644d3
facundo <00facuperez@gmail.com>
2026-01-27 18:55:01 -0300

    clase app agregada

---
+++ src/app/Aplicacion.java
@@ -0,0 +1,25 @@
+package app;
+
+import dto.Peticion;
+import interfaces.BaseParser;
+import interfaces.Servicio;
+import parseadores.OperacionParser;
+
+public class Aplicacion {
+    private ServiceLocator servicios;
+    private BaseParser<Peticion> lector;
+
+    public Aplicacion(ServiceLocator servicios) {
+        this.servicios = servicios;
+        this.lector = new OperacionParser();
+    }
+
+    public void iniciar() {
+
+        Peticion op = lector.leerCadena("usuarios login user=facu,pass=1234");
+
+        Servicio servicio = servicios.buscarServicio(op.getServicio());
+
+        servicio.ejecutar(op);
+    }
+}
