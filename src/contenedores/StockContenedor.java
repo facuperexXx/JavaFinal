dd1f133ac873fc000e7c544341337944b7170206
facundoPerez <00facuperez@gmail.com>
2026-02-05 00:21:44 -0300

    agregado contenedor de stock

---
+++ src/contenedores/StockContenedor.java
@@ -0,0 +1,23 @@
+package contenedores;
+
+import models.Articulo;
+
+public class StockContenedor extends BaseContenedor<Articulo>{
+
+    private static StockContenedor singleton;
+
+    public static StockContenedor getInstance() {
+        if(singleton == null) return new StockContenedor();
+        else return singleton;
+    }
+
+    @Override
+    protected int datoClave(Articulo ref) {
+        return ref.getCodigo();
+    }
+
+    @Override
+    protected void modificarRegistro(Articulo registro, Articulo datosNuevos) {
+
+    }
+}
