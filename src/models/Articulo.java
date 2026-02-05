1918c81b28f11d42b7e708529528e3ad9bcf4353
facundoPerez <00facuperez@gmail.com>
2026-02-04 23:29:38 -0300

    clase articulo para stock definida

---
+++ src/models/Articulo.java
@@ -0,0 +1,50 @@
+package models;
+
+public class Articulo {
+    private String codigo;
+    private String nombre;
+    private int precio;
+    private int stock;
+
+    public Articulo() {}
+
+    public Articulo(String codigo, String nombre, int precio, int stock) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Articulo [ " + codigo + " - " + nombre + " - Precio: $" + precio + " - Stock: " + stock + " ]";
+    }
+}
